aa71ee10ecd1d2289a1cb155c645bbafe9b85606
Marc Nitzsche <marc.nitzsche@live.de>
2016-05-18 10:54:00 +0200

    Rename app.jsx to App.jsx

--- client/main.jsx
+++ client/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 
-import App from '../imports/ui/app.jsx';
+import App from '../imports/ui/App.jsx';
 
 Meteor.startup(() => {
   render(<App />, document.getElementById('render-target'));
